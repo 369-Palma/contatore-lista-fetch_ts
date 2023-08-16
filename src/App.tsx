22aa9ff5625231a399ff18805d35c4265c58d3cd
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-08-16 13:02:07 +0200

    componenti

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+/* OPPURE COSI':const App = () => <>Hello React</>; */
+
+import { Empty } from "./components/Empty";
+import { Total } from "./components/Total";
+
+/* function App() {
+  return <>Hello</>;
+} */
+
 function App() {
-  return <></>;
+  const totalProd = 5;
+
+  function inc(e: React.MouseEvent) {
+    console.log("Ciao", e.clientY, e.clientX);
+  }
+
+  /*   const renderTotal = () => (
+    <div>
+      <h1>There are {totalProd} </h1>
+    </div>
+  );
+
+  function renderEmpty() {
+    return (
+      <div>
+        <em>No items </em>
+      </div>
+    );
+  } */
+
+  /*  return <>{totalProd > 0 ? renderTotal() : renderEmpty()}</>; */
+  return (
+    <>
+      {totalProd > 0 ? <Total value={totalProd} /> : <Empty />}
+      <button onClick={inc}>Add</button>
+    </>
+  );
 }
 
 export default App;
